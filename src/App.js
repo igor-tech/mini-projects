3beba59b273dbf95e659a5031e71043a25726016
igorek <igor20513@gmail.com>
2022-08-16 02:18:02 +0500

    users

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import {Success} from './components/Success';
+import {Users} from './components/Users';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+    const [users, setUsers] = useState([])
+    const [invites, setInvites] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [success, setSuccess] = useState(false)
+    const [searchValue, setSearchValue] = useState('')
+
+
+    const onClickInvite = ( id) => {
+        if (invites.includes(id)) {
+            setInvites(prev => prev.filter(_id => _id !== id))
+        } else {
+            setInvites(prev => [...prev, id])
+        }
+    }
+
+    const onClickInvites = () => {
+        setSuccess(true)
+    }
+
+    useEffect(() => {
+        fetch('https://reqres.in/api/users')
+            .then((res) => res.json())
+            .then((json) => {
+                setUsers(json.data)
+            })
+            .catch(err => {
+                console.warn(err);
+                alert('Ошибка при получении пользователей')
+            }).finally(() => setIsLoading(false))
+
+    }, [])
+
+    const onChangeSearchValue = (event) => {
+        setSearchValue(event.target.value)
+    }
+
+
+    return (
+        <div className="App">
+            {
+                success
+                    ? <Success count={invites.length} />
+                    : <Users
+                        onChangeSearchValue={onChangeSearchValue}
+                        searchValue={searchValue}
+                        items={users}
+                        isLoading={isLoading}
+                        onClickInvite={onClickInvite}
+                        invites={invites}
+                        onClickInvites={onClickInvites}
+                    />
+            }
+        </div>
+    );
 }
 
 export default App;
